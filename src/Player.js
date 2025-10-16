738a98fea4f47aa761f5c308234e22c6bfb35e78
jiyxxn <jiyunk@naver.com>
2025-10-16 11:42:58 +0900

    add: 플레이어 추가

---
+++ src/Player.js
@@ -0,0 +1,36 @@
+import { Stuff } from "./Stuff";
+import { cm1 } from "./common";
+import {AnimationMixer, BoxGeometry, Mesh, MeshBasicMaterial} from "three";
+
+export class Player extends Stuff {
+  constructor(info) {
+    super(info);
+
+    cm1.gltfLoader.load(
+      'models/ilbuni.glb',
+      glb => {
+        this.modelMesh = glb.scene.children[0];
+        this.modelMesh.position.set(this.x, this.y, this.z);
+        this.modelMesh.rotation.set(this.rotationX, this.rotationY, this.rotationZ);
+        this.modelMesh.castShadow = true;
+        cm1.scene.add(this.modelMesh);
+
+        this.modelMesh.animations = glb.animations;
+        cm1.mixer = new AnimationMixer(this.modelMesh);
+        this.actions = [];
+        this.actions[0] = cm1.mixer.clipAction(this.modelMesh.animations[0]); // default
+        this.actions[1] = cm1.mixer.clipAction(this.modelMesh.animations[1]); // fall
+        this.actions[2] = cm1.mixer.clipAction(this.modelMesh.animations[2]); // jump
+
+        this.actions[0].play();
+      }
+    );
+
+    // this.mesh = new Mesh(this.geometry, this.material);
+    // this.mesh.position.set(this.x, this.y, this.z);
+    // this.mesh.castShadow = true;
+    // this.mesh.receiveShadow = true;
+    //
+    // cm1.scene.add(this.mesh);
+  }
+}
