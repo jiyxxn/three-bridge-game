f159ad684668696b4f84a2f37c255a2e6c60e9fd
jiyxxn <jiyunk@naver.com>
2025-10-16 11:09:43 +0900

    add: 유리 추가

---
+++ src/Glass.js
@@ -0,0 +1,21 @@
+import { Stuff } from "./Stuff";
+import {cm1, geo, mat} from "./common";
+import {Mesh} from "three";
+
+export class Glass extends Stuff {
+  constructor(info) {
+    super(info);
+
+    this.geometry = geo.glass;
+    this.material = mat.glass;
+
+    this.mesh = new Mesh(this.geometry, this.material);
+    this.mesh.position.set(this.x, this.y, this.z);
+    this.mesh.castShadow = true;
+    this.mesh.receiveShadow = true;
+
+    this.type = info.type;
+
+    cm1.scene.add(this.mesh);
+  }
+}
