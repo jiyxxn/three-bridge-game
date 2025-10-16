de19737402a6d1fe1afe229e778bb543c8e8172a
jiyxxn <jiyunk@naver.com>
2025-10-16 10:54:40 +0900

    add: 공통 세팅 추가

---
+++ src/common.js
@@ -0,0 +1,29 @@
+import {BoxGeometry, MeshPhongMaterial, Scene, SphereGeometry} from "three";
+
+
+export const cm1 = {
+  canvas: document.querySelector('#three-canvas'),
+  scene: new Scene(),
+};
+
+export const cm2 = {
+  backgroundColor: '#3e1322',
+  lightColor: '#ffe9ac',
+  floorColor: '#111',
+  pillarColor: '#071d28',
+  barColor: '#441c1d',
+};
+
+export const geo = {
+  floor: new BoxGeometry(200, 1, 200),
+  pillar: new BoxGeometry(5, 10, 5),
+  bar: new BoxGeometry(0.1, 0.3, 1.2 * 21),
+  sideLight: new SphereGeometry(0.1, 6, 6),
+};
+
+export const mat = {
+  floor: new MeshPhongMaterial({ color: cm2.floorColor }),
+  pillar: new MeshPhongMaterial({ color: cm2.pillarColor }),
+  bar: new MeshPhongMaterial({ color: cm2.barColor }),
+  sideLight: new MeshPhongMaterial({ color: cm2.lightColor })
+};
