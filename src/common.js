46b201efc63c301afb822f1e4dc648cb1908850d
jiyxxn <jiyunk@naver.com>
2025-10-16 11:10:10 +0900

    feat: 유리 세팅 추가 및 씬 반영

--- src/common.js
+++ src/common.js
@@ -12,6 +12,7 @@ export const cm2 = {
   floorColor: '#111',
   pillarColor: '#071d28',
   barColor: '#441c1d',
+  glassColor: '#9fdfff'
 };
 
 export const geo = {
@@ -19,11 +20,17 @@ export const geo = {
   pillar: new BoxGeometry(5, 10, 5),
   bar: new BoxGeometry(0.1, 0.3, 1.2 * 21),
   sideLight: new SphereGeometry(0.1, 6, 6),
+  glass: new BoxGeometry(1.2, 0.05, 1.2),
 };
 
 export const mat = {
   floor: new MeshPhongMaterial({ color: cm2.floorColor }),
   pillar: new MeshPhongMaterial({ color: cm2.pillarColor }),
   bar: new MeshPhongMaterial({ color: cm2.barColor }),
-  sideLight: new MeshPhongMaterial({ color: cm2.lightColor })
+  sideLight: new MeshPhongMaterial({ color: cm2.lightColor }),
+  glass: new MeshPhongMaterial({
+    color: cm2.glassColor,
+    transparent: true,
+    opacity: 0.1,
+  })
 };
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {Pillar} from "./Pillar";
 import {Floor} from "./Floor";
 import {Bar} from "./Bar";
 import {SideLight} from "./SideLight";
+import {Glass} from "./Glass";
 
 // ----- 주제: The Bridge 게임 만들기
 
@@ -61,6 +62,7 @@ controls.enableDamping = true;
 
 // 물체 만들기
 const glassUnitSize = 1.2;
+const numberOfGlass = 10;
 
 // 바닥
 const floor = new Floor({
@@ -101,6 +103,39 @@ for (let i = 0; i < 49; i++) {
     container: bar4.mesh,
     z: i * 0.5 - glassUnitSize * 10,})
 }
+
+// 유리판
+let glassTypeNumber = 0;
+let glassTypes = [];
+
+for (let i = 0; i < numberOfGlass; i++) {
+  glassTypeNumber = Math.round(Math.random());
+  switch (glassTypeNumber) {
+    case 0:
+      glassTypes = ['normal', 'strong'];
+      break;
+    case 1:
+      glassTypes = ['strong', 'normal'];
+      break;
+  }
+
+  const glass1 = new Glass({
+    name: `glass-${glassTypes[0]}`,
+    x: -1,
+    y: 10.5,
+    z: i * glassUnitSize * 2 - glassUnitSize * 9,
+    type: glassTypes[0]
+  });
+  const glass2 = new Glass({
+    name: `glass-${glassTypes[1]}`,
+    x: 1,
+    y: 10.5,
+    z: i * glassUnitSize * 2 - glassUnitSize * 9,
+    type: glassTypes[1]
+  });
+}
+
+
 
 // 그리기
 const clock = new THREE.Clock();
